9f161732be3a316964701cb1030ca5283e591066
nascott <nascott94@outlook.com>
2021-10-15 12:09:25 -0600

    created two components and starting to build app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,45 @@
 import logo from "./logo.svg";
+import { Container, AppBar, Typography, Grow, Grid } from "@material-ui/core";
+import Student from "./components/showStudent/showStudent.js";
+import Create from "./components/createStudent/createStudent.js";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>nat</h1>
-      </header>
+      <Container maxWidth="lg">
+        <AppBar className={classes.appBar} position="static" color="inherit">
+          <Typography className={classes.heading} variant="h2" align="center">
+            {" "}
+            Student Create and Show
+          </Typography>
+        </AppBar>
+        <Grow in>
+          <Container>
+            <Grid container justify="space-between" alignItems="strect">
+              <Grid item xs={12} sm={7}>
+                <AppBar
+                  className={classes.appBar}
+                  position="static"
+                  color="inherit"
+                >
+                  <Student />
+                </AppBar>
+              </Grid>
+
+              <Grid item xs={12} sm={4}>
+                <AppBar
+                  className={classes.appBar}
+                  position="static"
+                  color="inherit"
+                >
+                  <Create />
+                </AppBar>
+              </Grid>
+            </Grid>
+          </Container>
+        </Grow>
+      </Container>
     </div>
   );
 }
